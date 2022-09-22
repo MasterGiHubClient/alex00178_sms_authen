0bb77b5a73ff8b45ccc7333fff5db74b4f1d7725
SGCEntertainment <zloalways6@gmail.com>
2022-09-22 03:14:51 +0300

    update

--- Assets/alex00178_sms_authen/Scripts/UsersManager.cs
+++ Assets/alex00178_sms_authen/Scripts/UsersManager.cs
@@ -1,19 +1,54 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
 
 public class UsersManager : MonoBehaviour
 {
-    //[SerializeField] bool enable;
-    //[SerializeField] UsersContainer usersContainer;
+    [SerializeField] UsersContainer usersContainer;
 
-    //private void OnValidate()
-    //{
-    //    if(!enable)
-    //    {
-    //        return;
-    //    }
+    private void Start()
+    {
+        StartCoroutine(DownloadDB(success => 
+        {
+            Debug.Log(success);
+        }));
+    }
 
-    //    enable = false;
-    //    string json = JsonUtility.ToJson(usersContainer);
-    //    Debug.Log(json);
-    //}
+    IEnumerator DownloadDB(Action<bool> success)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Get("https://api.github.com/repos/MasterGiHubClient/alex00178_sms_authen/contents/db.json");
+        webRequest.SetRequestHeader("Authorization", "Bearer " + "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");
+        yield return webRequest.SendWebRequest();
+
+        if(webRequest.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(webRequest.error);
+            success.Invoke(false);
+        }
+        else
+        {
+            Responce responce = JsonUtility.FromJson<Responce>(webRequest.downloadHandler.text);
+
+            webRequest = UnityWebRequest.Get(responce.download_url);
+            yield return webRequest.SendWebRequest();
+            
+            if(webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log(webRequest.error);
+                success.Invoke(false);
+            }
+            else
+            {
+                usersContainer = JsonUtility.FromJson<UsersContainer>(webRequest.downloadHandler.text);
+                success.Invoke(true);
+            }
+        }
+    }
+
+    [Serializable]
+    public class Responce
+    {
+        public string download_url;
+    }
 }
